62af25d79fad3b4a5d29fd5dd8e6a97e667c607a
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2022-09-13 17:25:46 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = "7556932"
 API_HASH = "6b99e012069f373abbcac581d3cdd6db"
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
+BOT_TOKEN = "5504367878:AAHq7y5bO75Zym6MDbYb_46gQflhm6h6Pu8"
+BOT_UN = "Awesome_Files_Compressor_Bot"
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
